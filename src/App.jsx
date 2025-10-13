c532d65b7498cc399fb5880b3a477cd1fa68e0a2
kaidoverse <kaidoverse@gmail.com>
2025-10-13 13:58:27 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+
+    <div className='container mx-auto flex  justify-between items-center py-3 px-6'>
+      <h1>hi</h1>
+    </div>
+
+  );
+}
+
+export default App;
+
+
+
